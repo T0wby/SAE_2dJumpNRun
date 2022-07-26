138069637476fff65f87bfda5af1f88f3574860d
T0wby <tobiasnuehlen@gmx.de>
2022-07-26 20:10:08 +0200

    -First try at saving

--- Assets/Scripts/Manager/SaveGameManager.cs
+++ Assets/Scripts/Manager/SaveGameManager.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class SaveGameManager : MonoBehaviour
 {
     public static SaveGameManager Instance { get; private set; }
+
+    [SerializeField] private SO_GameSettings _gameSettings;
+    [SerializeField] private SO_LevelObjects _levelObjects;
+
+    private string _saveGameSettings;
+    private string _saveGameObjects;
 
     void Awake()
     {
@@ -17,5 +24,30 @@ public class SaveGameManager : MonoBehaviour
         }
         DontDestroyOnLoad(gameObject);
 
+    }
+
+    public void Save()
+    {
+        _saveGameSettings = JsonUtility.ToJson(_gameSettings, true);
+        _saveGameObjects = JsonUtility.ToJson(_levelObjects, true);
+
+        string filePath = $"{Application.dataPath}/savegamesettings.cgsav";
+        string filePathTwo = $"{Application.dataPath}/savegameobjects.cgsav";
+
+        using (StreamWriter writer = File.CreateText(filePath))
+        {
+            writer.Write(_saveGameSettings);
+        }
+
+        using (StreamWriter writer = File.CreateText(filePathTwo))
+        {
+            writer.Write(_saveGameSettings);
+        }
+
+    }
+
+    public void Load()
+    {
+        
     }
 }
